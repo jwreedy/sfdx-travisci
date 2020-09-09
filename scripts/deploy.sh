a820dbc3a96b58b684a5d0874ed25b8cd399bedc
John Reedy <jreedy@ascap.com>
2020-09-09 13:47:24 -0400

    added entire script back

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -38,16 +38,16 @@ sfdx force:source:convert -r ./force-app/ -d ./mdapi/
 return_val=$?; error_handler; message="Creating mdapi directory"
 
 # run local tests
-#echo "$script_name INFO: Running Local Tests"
-#sfdx force:mdapi:deploy -c -d ./mdapi -u dev -w 10 >$tests_log
+echo "$script_name INFO: Running Local Tests"
+sfdx force:mdapi:deploy -c -d ./mdapi -u dev -w 10 >$tests_log
 #sfdx force:apex:test:run -c -u dev -r human -w 10 >$tests_log
-#if grep -q 'Outcome              Failed' $tests_log; then
-#  echo "$script_name ERROR: An Error occured while running local tests:"
-#  grep 'Fail ' $tests_log
-#  echo '$script_name ERROR: Exiting $script_name due to error(s)'
+if grep -q 'Outcome              Failed' $tests_log; then
+  echo "$script_name ERROR: An Error occured while running local tests:"
+  grep 'Fail ' $tests_log
+  echo '$script_name ERROR: Exiting $script_name due to error(s)'
   # cat local_tests.log
-#  exit 1
-##fi
+  exit 1
+fi
 #echo "$script_name INFO: SUCCESS: Local Tests Successfully Completed"
 echo "$script_name INFO: Deploying to .dev Sandbox"
 sfdx force:mdapi:deploy -d ./mdapi -u dev -w 10 >$deployment_log
