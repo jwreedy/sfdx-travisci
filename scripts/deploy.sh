47e3ec581e7de97b3550f4881fdfb68169f83632
John Reedy <jreedy@ascap.com>
2020-09-09 11:47:09 -0400

    added user to report command and updated to new code coverage

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,4 @@ sfdx force:auth:sfdxurl:store -f assets/dev-login.txt -a dev
 sfdx force:source:convert -r ./force-app/ -d ./mdapi/
 sfdx force:apex:test:run -c -u dev -r human -w 10
 sfdx force:mdapi:deploy -d ./mdapi -u dev
-sfdx force:mdapi:deploy:report -w 10
-
+sfdx force:mdapi:deploy:report -w 10 -u dev
