1b2bf2b70db29667fa2443b8e6242866149209d9
John Reedy <jreedy@ascap.com>
2020-09-09 11:53:03 -0400

    added wait to deploy command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 sfdx force:auth:sfdxurl:store -f assets/dev-login.txt -a dev
 sfdx force:source:convert -r ./force-app/ -d ./mdapi/
 sfdx force:apex:test:run -c -u dev -r human -w 10
-sfdx force:mdapi:deploy -d ./mdapi -u dev - w 10
+sfdx force:mdapi:deploy -d ./mdapi -u dev -w 10
 sfdx force:mdapi:deploy:report -w 10 -u dev
