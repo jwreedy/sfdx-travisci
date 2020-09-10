323821a8cf34ab2219df3a818b5b28f9747e14c7
John Reedy <jreedy@ascap.com>
2020-09-10 17:14:13 -0400

    added run local tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -33,17 +33,19 @@ sfdx force:auth:sfdxurl:store -f assets/dev-login.txt -a dev
 
 # remove madapi directory and recreate
 echo "$script_name INFO: Creating mdapi directory"
-rm -r mdapi
+#rm -r mdapi
 sfdx force:source:convert -r ./force-app/ -d ./mdapi/
 return_val=$?; error_handler; message="Creating mdapi directory"
 
 # run local tests
 echo "$script_name INFO: Running Local Tests"
-sfdx force:mdapi:deploy -c -d ./mdapi -u dev -w 10 >$tests_log
+sfdx force:mdapi:deploy -c -d ./mdapi -l RunLocalTests -u dev -w 10 >$tests_log
 #sfdx force:apex:test:run -c -u dev -r human -w 10 >$tests_log
-if grep -q 'Outcome              Failed' $tests_log; then
+if grep -q 'Failed' $tests_log; then
   echo "$script_name ERROR: An Error occured while running local tests:"
-  grep 'Fail ' $tests_log
+  echo "**"
+  grep 'Failed ' $tests_log
+  echo "**"
   echo '$script_name ERROR: Exiting $script_name due to error(s)'
   # cat local_tests.log
   exit 1
