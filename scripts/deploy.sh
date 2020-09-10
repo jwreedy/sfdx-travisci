dcc3eb229f0f0a299e21eccd6d6e201d2a0ba0ce
John Reedy <jreedy@ascap.com>
2020-09-10 17:56:55 -0400

    added error handling and fixed message postioining

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,30 +32,37 @@ echo "$script_name INFO: logging in to org"
 sfdx force:auth:sfdxurl:store -f assets/dev-login.txt -a dev
 
 # remove madapi directory and recreate
+message="Creating mdapi directory"
 echo "$script_name INFO: Creating mdapi directory"
 rm -r mdapi
 sfdx force:source:convert -r ./force-app/ -d ./mdapi/
-return_val=$?; error_handler; message="Creating mdapi directory"
+return_val=$?; error_handler
 
 # run local tests
+message="Running Local Tests - check deployment status in org for errors"
 echo "$script_name INFO: Running Local Tests"
 sfdx force:mdapi:deploy -c -d ./mdapi -l RunLocalTests -u dev -w 10 >$tests_log
+return_val=$?; error_handler
+
 #sfdx force:apex:test:run -c -u dev -r human -w 10 >$tests_log
-if grep -q 'Failed' $tests_log; then
+if grep -q 'Failed\ERROR' $tests_log; then
   echo "$script_name ERROR: An Error occured while running local tests:"
   echo "**"
-  grep 'Failed' $tests_log
+  grep 'Failed\ERROR' $tests_log
   echo "**"
   echo '$script_name ERROR: Exiting $script_name due to error(s)'
   # cat local_tests.log
   exit 1
 fi
 #echo "$script_name INFO: SUCCESS: Local Tests Successfully Completed"
+message="Deploying to dev - check deployment status in org for errors"
 echo "$script_name INFO: Deploying to .dev Sandbox"
 sfdx force:mdapi:deploy -d ./mdapi -u dev -w 10 >$deployment_log
-if grep -q 'Error' $deployment_log; then
+return_val=$?; error_handler
+
+if grep -q 'Error\ERROR' $deployment_log; then
   echo "$script_name ERROR: An Error occured while deploying metadata"
-  grep 'Error ' $deployment_log
+  grep 'Error\ERROR' $deployment_log
   echo '$script_name ERROR: Exiting $script_name due to error(s)'
   exit 1
 fi
